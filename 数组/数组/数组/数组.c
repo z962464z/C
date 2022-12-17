ea5ddc49db1ec540766bffb5dbf8361c07f4d869
z962464z <114372810+z962464z@users.noreply.github.com>
2022-12-17 10:44:00 +0800

    提交代码

--- 数组/数组/数组/数组.c
+++ 数组/数组/数组/数组.c
@@ -4,6 +4,9 @@
 #include <stdio.h>
 #include <string.h>
 
+
+//一维数组
+
 //int main()
 //{
 //	//int arr[] = { 1, 2, 3 };//不完全初始化，剩下的元素默认初始化为0
@@ -61,15 +64,118 @@
 //	return 0;
 //}
 
+//int main()
+//{
+//	int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	int i = 0;
+//	for (i = 0; i < sz; i++)
+//	{
+//		printf("&arr[%d]=%p\n", i, &arr[i]);// %p 打印地址
+//		//数组在内存中是连续存放的，从低地址->高地址
+//	}
+//	return 0;
+//}
+
+//二维数组
+
+//int main()
+//{
+//	int arr[3][4] = { { 1, 2, 3 }, { 4, 5 } };
+//	int i = 0;
+//	for (i = 0; i < 3; i++)
+//	{
+//		int j = 0;
+//		for (j = 0; j < 4; j++)
+//		{
+//			printf("%d ", arr[i][j]);
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	int arr[3][4] = { { 1, 2, 3 }, { 4, 5 } };
+//	int i = 0;
+//	for (i = 0; i < 3; i++)
+//	{
+//		int j = 0;
+//		for (j = 0; j < 4; j++)
+//		{
+//			printf("&arr[%d][%d]=%p\n",i,j, &arr[i][j]);
+//		}
+//
+//	}
+//	return 0;
+//}
+
+
+//冒泡排序
+
+//void bubble_sort(int arr[], int sz)
+//{
+//	int i = 0;
+//	for (i = 0; i < sz-1; i++)//确定冒泡排序的趟数
+//	{
+//		int flag = 1;//假设这一趟排序的数据已经有序
+//		int j = 0;
+//		for (j = 0; j < sz - 1 - i;j++)//每一趟
+//		{
+//			if (arr[j]>arr[j+1])
+//			{
+//
+//				int tmp = arr[j];
+//				arr[j] = arr[j+1];
+//				arr[j+1] = tmp;
+//				flag = 0;//本趟排序的数据不完全有序
+//			}
+//		}
+//		if (flag == 1)
+//		{
+//			break;
+//		}
+//	}
+//}
+//int main()
+//{
+//	int arr[] = { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 };
+//	//对arr进行排序，排成升序
+//	int i = 0;
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	bubble_sort(arr, sz);//冒泡排序函数
+//	//arr是数组，我们对数组arr进行传参，实际上传过去的是数组arr首元素的地址
+//	for (i = 0; i < sz; i++)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//
+//	return 0;
+//}
+
+
+
 int main()
 {
-	int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
-	int sz = sizeof(arr) / sizeof(arr[0]);
-	int i = 0;
-	for (i = 0; i < sz; i++)
-	{
-		printf("&arr[%d]=%p\n", i, &arr[i]);// %p 打印地址
-		//数组在内存中是连续存放的，从低地址->高地址
-	}
+	int arr[] = { 1, 2, 3, 4, 5, 6 };
+	printf("%p\n", arr);
+	printf("%p\n", arr+1);
+
+	printf("%p\n", arr[0]);
+	printf("%p\n", arr[0]+1);
+
+	printf("%p\n", &arr);
+	printf("%p\n", &arr+1);
+
+
+	//int sz = sizeof(arr) / sizeof(arr[0]);
+	//1.sizeof（数组名） - 数组名表示整个数组，sizeof（数组名）计算的是整个数组的大小，单位是字节
+	//2. &数组名，数组名代表整个数组，&数组名，取出的是整个数组的地址
+
+	//printf("%p\n",arr);
+	//printf("%p\n",&arr[0]);
+	//printf("%p\n",*arr);
+
 	return 0;
 }
