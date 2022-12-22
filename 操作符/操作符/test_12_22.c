dc3370f238f714db189b990ff5e74be9113c7827
z962464z <114372810+z962464z@users.noreply.github.com>
2022-12-22 10:14:37 +0800

    提交代码

---
+++ 操作符/操作符/test_12_22.c
@@ -0,0 +1,32 @@
+#define  _CRT_SECURE_NO_WARNINGS 
+
+#include <stdio.h>
+
+//int main()
+//{
+//	int a = 5;
+//	int b = 2;
+//	int c = 5 / 2;
+//	int d = 5 % 2;
+//	printf("%d\n", c);
+//	printf("%d\n", d);
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int a = 16;
+//	int b =a >> 1;
+//	printf("%d\n", b);
+//	return 0;
+//}
+
+
+int main()
+{
+	int a = 16;
+	int b = a << 1;
+	printf("%d\n", b);
+	return 0;
+}
