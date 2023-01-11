11ad257e7b1a6c67c0955498ce33e89393647b96
z962464z <114372810+z962464z@users.noreply.github.com>
2023-01-11 21:41:00 +0800

    提交代码

--- 指针/指针/指针.c
+++ 指针/指针/指针.c
@@ -41,15 +41,64 @@
 //	return 0;
 //}
 
+//
+//int main()
+//{
+//	int arr[10] = { 0 };
+//	int*p = arr;
+//	int i = 0;
+//	for (i = 0; i < 10; i++)
+//	{
+//		*(p + i) = 1;
+//	}
+//	return 0;
+//}
 
+
+//int main()
+//{
+//	int arr[10] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+//	int i = 0;
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	//int*p = arr;
+//	int*p = &arr[9];
+//	/*for (i = 0; i < 10; i++)
+//	{
+//		printf("%d ",* p);
+//		p = p + 1;
+//	}*/
+//
+//	for (i = 0; i < 10; i++)
+//	{
+//		printf("%d ", *p);
+//		p -= 1;
+//	}
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int arr[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+//	printf("%d\n", &arr[9] - &arr[0]);
+//	return 0;
+//}
+
+
+int my_strlen(char*str)
+{
+	char*start = str;
+	char*end = str;
+	while (*end != '\0')
+	{
+		end++;
+	}
+	return end - start;
+}
 int main()
 {
-	int arr[10] = { 0 };
-	int*p = arr;
-	int i = 0;
-	for (i = 0; i < 10; i++)
-	{
-		*(p + i) = 1;
-	}
+	char arr[] = "bit";
+	int len = my_strlen(arr);
+	printf("%d\n", len);
 	return 0;
 }
