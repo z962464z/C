00a84ca8f5296dc99259e13470618f7f03538bd5
z962464z <114372810+z962464z@users.noreply.github.com>
2023-03-19 20:42:45 +0800

    提交代码

--- tset_3_19/tset_3_19/tset_3_19.c
+++ tset_3_19/tset_3_19/tset_3_19.c
@@ -98,18 +98,18 @@
 
 
 
-void print(int *p, int sz)//用指针打印数组
-{
-	int i = 0;
-	for (i = 0; i < sz; i++)
-	{
-		printf("%d", *(p + i));
-	}
-}
-int main()
-{
-	int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-	int sz = sizeof(arr) / sizeof(arr[0]);
-	print(arr, sz);
-	return 0;
-}
+//void print(int *p, int sz)//用指针打印数组
+//{
+//	int i = 0;
+//	for (i = 0; i < sz; i++)
+//	{
+//		printf("%d", *(p + i));
+//	}
+//}
+//int main()
+//{
+//	int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	print(arr, sz);
+//	return 0;
+//}
